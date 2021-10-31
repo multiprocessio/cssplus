6ce6d65226523719ed6110023b53b6c36accfe2c
Phil Eaton <phil@eatonphil.com>
2021-10-31 03:51:43 +0000

    Rebuild

--- dist/cssplus.d.ts
+++ dist/cssplus.d.ts
@@ -1,14 +1,14 @@
 export declare const SETTINGS: {
-    DEBUG: boolean;
+  DEBUG: boolean;
 };
 export interface Declaration {
-    type: "declaration";
-    property: string;
-    value: string;
+  type: "declaration";
+  property: string;
+  value: string;
 }
 export interface Rule {
-    type: "rule";
-    selectors: Array<string>;
-    declarations: Array<Declaration | Rule>;
+  type: "rule";
+  selectors: Array<string>;
+  declarations: Array<Declaration | Rule>;
 }
 export declare function transform(cssp: string): string;
